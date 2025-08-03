45093ea2121b5dd03889c87a309192b0cf8cf155
chelsea.wang86@gmail.com <chelsea.wang86@gmail.com>
2025-08-03 16:23:15 -0400

    fixed comments

--- script.js
+++ script.js
@@ -3,7 +3,6 @@ const genreCounts = {
     pop: 0,
     hiphop: 0,
     r_and_b: 0,
-    // add more genres here
 };
 let bestSongsIndex = 0;
 const bestSongs = [];
@@ -47,7 +46,7 @@ function swipeRight() {
             document.getElementById(cards[index + 1].id).classList.remove('hidden');    
             document.getElementById(cards[nextIndex].id).classList.remove('hidden');
             audioSource.src = cards[nextIndex].audio + ".mp3";
-            audio.load(); // reloads the new source
+            audio.load();
         } else {
             document.getElementById('playPause').disabled = true;
             document.getElementById('yes').classList.add('hidden');
@@ -56,7 +55,7 @@ function swipeRight() {
             document.getElementById(cards[index].id).classList.add('hidden');
             for (let genre of Object.keys(genreCounts)) {
                 if (genreCounts[genre] === 2) {
-                    bestSongs.push(genre); // push the genre name (key), not the number
+                    bestSongs.push(genre);
                 }
             }
             if (bestSongs.length > 1) {
@@ -68,13 +67,6 @@ function swipeRight() {
         index++;
     }, 600
     )
-}
-function getTopGenresWithTies(genreCounts) {
-    const entries = Object.entries(genreCounts);
-    const max = Math.max(...entries.map(entry => entry[1]));
-    return entries
-        .filter(entry => entry[1] === max) // only those with highest count
-        .map(entry => entry[0]);           // return just genre names
 }
 
 function swipeLeft() {
@@ -111,7 +103,7 @@ function swipeLeft() {
             document.getElementById(cards[index].id).classList.add('hidden');
             for (let genre of Object.keys(genreCounts)) {
                 if (genreCounts[genre] === 2) {
-                    bestSongs.push(genre); // push the genre name (key), not the number
+                    bestSongs.push(genre);
                 }
             }
             if (bestSongs.length > 1) {
